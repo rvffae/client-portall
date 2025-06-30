78d28a670dcfa2865fa3d746081878d533ea7f94
rvffae <raffaele691@icloud.com>
2025-06-30 12:31:20 +0200

    navbar fix redirections + style project

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,7 @@ import './styles/communication.scss';
 import './styles/googlecalendar.scss';
 import './styles/form.scss';
 import './styles/client.scss';
+import './styles/project.scss';
 
 import './js/calendar.js';
 import './js/dashboard.js';
